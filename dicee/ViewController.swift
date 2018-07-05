e1bb454cdb1ea564595dd07ae25b0d7b55fad5bd
Jay Letheby <jay.letheby@gmail.com>
2018-07-05 15:44:37 +1000

    Initial Commit

---
+++ dicee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dicee
+//
+//  Created by Jay Letheby on 5/7/18.
+//  Copyright © 2018 Jay Letheby solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
